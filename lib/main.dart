e77cf22ff0118906624dfd3e344560ea012971a5
Dilum <sudheeradilum@gmail.com>
2025-05-11 10:31:58 +0530

    status bar updates

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/services.dart';
 import 'package:water_quality/firebase_options.dart';
 import 'package:water_quality/main_screen.dart';
 
@@ -14,12 +15,18 @@ void main() async {
   runApp(const WaterQualityApp());
 }
 
-
 class WaterQualityApp extends StatelessWidget {
   const WaterQualityApp({super.key});
 
   @override
   Widget build(BuildContext context) {
+
+    SystemChrome.setSystemUIOverlayStyle(
+      const SystemUiOverlayStyle(
+        statusBarColor: Color(0xFF1E2247),
+        statusBarIconBrightness: Brightness.light,
+      ),
+    );
     return MaterialApp(
       title: 'Water Quality Monitor',
       debugShowCheckedModeBanner: false,
